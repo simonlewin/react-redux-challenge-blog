a7898767526d1f06c345244bf3db9586b4925524
Simon Lewin <simonlewin@hotmail.com>
2018-06-12 16:22:25 +0100

    Added redux and react-redux

---
+++ src/data/initial.js
@@ -0,0 +1,27 @@
+// create some dummy articles
+// store them in an object
+// so that we can easily reference each one by its id
+const initial = { 
+  articles: {
+      1: {
+          id: 1,
+          title: "Post #1",
+          article: "<p>Blah blah blah</p>",
+          comments: [
+              { email: "bob@bob.com", comment: "Great blog post" },
+          ],
+          tags: ["blah", "monkeys"],
+      },
+      2: {
+          id: 2,
+          title: "Post #2",
+          article: "<p>Blah blah blah</p>",
+          comments: [
+              { email: "sandi@sandi.com", comment: "I disagree, but not in an agressive and unpleasant manner" },
+          ],
+          tags: ["blah", "fish-fingers"],
+      },
+  }
+};
+
+export default initial
--- src/index.js
+++ src/index.js
@@ -5,38 +5,34 @@ import ReactDOM from "react-dom";
 import history from "./history";
 import { Router } from "react-router-dom";
 
+// redux
+import { createStore } from "redux";
+
+import reducer from './data/reducer'
+import initial from './data/initial'
+
 // import the App component
 import App from "./App";
 
-// create some dummy articles
-// store them in an object
-// so that we can easily reference each one by its id
-const articles = {
-    1: {
-        id: 1,
-        title: "Post #1",
-        article: "<p>Blah blah blah</p>",
-        comments: [
-            { email: "bob@bob.com", comment: "Great blog post" },
-        ],
-        tags: ["blah", "monkeys"],
-    },
-    2: {
-        id: 2,
-        title: "Post #2",
-        article: "<p>Blah blah blah</p>",
-        comments: [
-            { email: "sandi@sandi.com", comment: "I disagree, but not in an agressive and unpleasant manner" },
-        ],
-        tags: ["blah", "fish-fingers"],
-    },
-};
+// react-redux
+import { Provider } from "react-redux";
 
+const store = createStore(
+	reducer,
+	initial,
+	window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+let articles = store.getState().articles;
+
+// wrap Router with Provider
 // wrap the app in Router
 // pass our articles through to App using props
 ReactDOM.render(
-    <Router history={ history }>
-        <App articles={ articles } />
-    </Router>,
-    document.getElementById("root"),
+	<Provider store={ store }>
+		<Router history={ history }>
+			<App articles={ articles } />
+		</Router>
+	</Provider>,
+	document.getElementById("root"),
 );
