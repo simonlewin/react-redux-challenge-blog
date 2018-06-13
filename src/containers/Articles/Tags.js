c6d7c6938a57b733a53be3c54600e9cdb001199e
Simon Lewin <simonlewin@hotmail.com>
2018-06-13 09:24:42 +0100

    Updated Tags.js to use includes() method and reformatted initial.js

--- src/containers/Articles/Tags.js
+++ src/containers/Articles/Tags.js
@@ -4,8 +4,8 @@ import Articles from "../../components/Articles/Articles";
 
 // the second argument passed to mapStateToProps represent the props passed in from the parent
 const mapStateToProps = (state, { tag }) => ({
-  // return articles with matching tag
-  articles: Object.values(state.articles).filter(article => article.tags.some(x => x === tag))
+  // return articles whose tags match tagname in props
+  articles: Object.values(state.articles).filter(article => article.tags.includes(tag))
 });
 
 // connect up mapStateToProps with the Article component
