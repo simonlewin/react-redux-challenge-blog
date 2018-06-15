24263f1bad399118ec1168c4ba7d7f6c4b4bf5a0
Simon Lewin <simonlewin@hotmail.com>
2018-06-15 16:13:53 +0100

    Updated Tags to use API action

--- src/containers/Articles/Tags.js
+++ src/containers/Articles/Tags.js
@@ -2,12 +2,20 @@ import { connect } from "react-redux";
 
 import Articles from "../../components/Articles/Articles";
 
+import { getTitles } from "../../data/actions/api"
+
 // the second argument passed to mapStateToProps represent the props passed in from the parent
 const mapStateToProps = (state, { tag }) => ({
   // return articles whose tags match tagname in props
-  articles: Object.values(state.articles).filter(article => article.tags.includes(tag))
+  articles: state.titles.filter(article => article.tags.includes(tag))
 });
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onLoad: () => dispatch(getTitles()),
+  };
+};
 
 // connect up mapStateToProps with the Article component
 // Article's props are now controlled by this file
-export default connect(mapStateToProps)(Articles);
+export default connect(mapStateToProps, mapDispatchToProps)(Articles);
